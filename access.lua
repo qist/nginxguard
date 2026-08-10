f85c6bb71a569459168bf93c7034e495970faa73
qist <87984115@qq.com>
2026-08-10 15:21:46 +0800

    fix: 白名单URL检测提前到UA检测之前,避免白名单路径被UA规则拦截

--- access.lua
+++ access.lua
@@ -212,11 +212,11 @@ function waf_main()
         return
     end
     if white_ip_check() then
+    elseif white_url_check() then
     elseif black_ip_check() then
     elseif user_agent_attack_check() then
     elseif cc_attack_check() then
     elseif cookie_attack_check() then
-    elseif white_url_check() then
     elseif url_attack_check() then
     elseif url_args_attack_check() then
     elseif post_attack_check() then
